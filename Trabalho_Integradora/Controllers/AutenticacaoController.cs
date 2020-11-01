092307c6376bedb6ff4ef5f5e446ff556d4f1da0
jpdotfranca <jpdotfranca@gmail.com>
2020-11-01 17:53:59 -0300

    Add project files.

---
+++ Trabalho_Integradora/Controllers/AutenticacaoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Authentication;
+
+namespace Trabalho_Integradora.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AutenticacaoController : ControllerBase
+    {
+
+        public ActionResult<string> Autenticar(string login, string senha)
+        {
+
+            if (string.IsNullOrWhiteSpace(login) || string.IsNullOrWhiteSpace(senha))
+                throw new AuthenticationException("O login ou a senha estão incorretos ou não foram informados. Por favor, verifique os dados e tente novamente.");
+
+            return Guid.NewGuid().ToString();
+        }
+
+    }
+}
